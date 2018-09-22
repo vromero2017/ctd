62b80488b2a16da8070391c3e5d9f47c9c023e60
Victor Romero <victor.romero2004@gmail.com>
2018-09-22 15:44:15 +0000

    second commit

---
+++ user_input_program/user_input_statistics.rb
@@ -0,0 +1,12 @@
+def main
+  # we need an array to store our data
+  # how do we assign an array to a variable?
+  loop do
+    # continue to ask for a number
+    # challenge: how do I check that may input is an integer and not a string?
+    break if value == '' # this will end the loop if the user input is blank
+  end
+  # output data
+end
+
+main if __FILE__ == $PROGRAM_NAME
