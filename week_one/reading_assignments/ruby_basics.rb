650d0196f580503efda01943c75753410581e87a
Victor Romero <victor.romero2004@gmail.com>
2018-09-24 01:15:55 +0000

    Modified

--- week_one/reading_assignments/ruby_basics.rb
+++ week_one/reading_assignments/ruby_basics.rb
@@ -1,6 +1,38 @@
+# answer each of the following to the best of your ability
+# there is always more than one way to code a response 😄
+# please use your knowledge of variable assignments in each response
+# when you are done, share your code with your instructor, push it to Github, or submit a pull request
+
+
+# output an integer
+
+integer = 12
+puts "this is and Integer #{integer}"
+
+
+# output the number of seconds in a week
+
 puts "This program will calculate he number of seconds in a given number of weeks"
 puts "let's get started..!!"
 print "Enter number of weeks to calculate: " 
 weeks = gets.chomp
 seconds = 60*60*24*7*weeks.to_i
 puts "There are #{seconds} seconds in  #{weeks} weeks "
+
+# output a float
+
+float = 12.02
+puts "this is and Integer #{float}"
+
+
+# output a string
+
+string = "This is an string"
+puts string
+
+
+# output concatination of strings
+
+string1 = "Hello"
+string2 = "World"
+puts string1 + string2
