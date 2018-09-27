828e8a540994dd09dbe8d7addbe79f0109d2f4f8
Victor Romero <victor.romero2004@gmail.com>
2018-09-27 05:47:46 +0000

    User Input

--- user_input_program/user_input_statistics.rb
+++ user_input_program/user_input_statistics.rb
@@ -1,24 +1,15 @@
-numbers_array=[]
-strings_array = Array.new
-#input = 0
+my_array=[]
+input = 0
 loop do
-  print "Enter a Number or a String, to exit press Enter..!! "
+  puts "Enter a Number, to exit press Enter..!! "
   input = gets.chomp
-  if input.to_i.to_s == input || input.to_f.to_s == input
-    numbers_array << input.to_i
-  else
-    strings_array << input
-  end
+  my_array << input.to_i
   break if input == ""
 end
-  puts "you have  #{numbers_array.count} items in the Numbers Array"  
-  average = numbers_array.count   
-  puts "The sum of all numbers in your Numbers Array is: #{numbers_array.sum}"
-  puts "The Numbers Array average is: #{numbers_array.sum / average.to_f}"
-  puts "--------------------------------------"
-  puts "you have #{strings_array.count} items in the Strings Array"
-  puts "Those are the items on your String Array #{strings_array.reverse.drop(1).reverse}"
-  
+  puts "you have  #{my_array.count - 1} items in the Array"  
+  average = my_array.count - 1   
+  puts "The sum of all numbers in your array is: #{my_array.sum}"
+  puts "The array average is: #{my_array.sum / average.to_f}"
 
 
 
---
+++ user_input_program/user_input_ver2.rb
@@ -0,0 +1,21 @@
+numbers_array=[]
+strings_array = Array.new
+#input = 0
+loop do
+  print "Enter a Number or a String, to exit press Enter..!! "
+  input = gets.chomp
+  if input.to_i.to_s == input || input.to_f.to_s == input
+    numbers_array << input.to_i
+  else
+    strings_array << input
+  end
+  break if input == ""
+end
+  puts "you have  #{numbers_array.count} items in the Numbers Array"  
+  average = numbers_array.count   
+  puts "The sum of all numbers in your Numbers Array is: #{numbers_array.sum}"
+  puts "The Numbers Array average is: #{numbers_array.sum / average.to_f}"
+  puts "--------------------------------------"
+  puts "you have #{strings_array.count} items in the Strings Array"
+  puts "Those are the items on your String Array #{strings_array.reverse.drop(1).reverse}"
+
